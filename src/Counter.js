1bead031e177354a4646f9b22cc4fda129993d23
Shirin Mohebi <89771218+Shirin-Mohebi-9943463@users.noreply.github.com>
2023-12-19 14:43:32 +0000

    first_commit

---
+++ src/Counter.js
@@ -0,0 +1,15 @@
+import React, {useState} from 'react';
+const Counter = () => {
+    // State hook goes here
+    const [count, setCount] = useState(0);
+    return(
+        // component jsx goes here
+        <div style={{textAlign:'center',marginTop:'50px'}}>
+            <h1 style={{fontSize:'2em'}}>Count:{count}</h1>
+            <button onClick={()=>setCount(count+1)}>Increment</button>
+            <button onClick={()=>setCount(count>0 ? count-1:0)}>Decrement</button>
+
+        </div>
+    );
+}
+export default Counter;
